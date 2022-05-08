95f775894f1347c6653c7b7875e03695ebcc2063
cristian <j.cristian.fedez@gmail.com>
2022-05-08 14:04:07 +0200

    Agregado log4j

--- src/main/java/pruebas/Solution.java
+++ src/main/java/pruebas/Solution.java
@@ -1,9 +1,12 @@
 package pruebas;
 
+import commons.logger.Logger;
+
 public class Solution {
 
   public static void main(String[] args) {
-    // TODO document why this method is empty
+    Logger logger = Logger.fromType(Solution.class);
+    logger.info("AAA");
   }
 }
 
